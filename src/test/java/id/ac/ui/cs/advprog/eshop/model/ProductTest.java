9f996d866619b8dc7e8c1f4c0d5c0ef969d502a9
nabilmuafa <nmuafa@gmail.com>
2024-02-06 23:34:36 +0700

    finish ProductTest

--- src/test/java/id/ac/ui/cs/advprog/eshop/model/ProductTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/ProductTest.java
@@ -13,8 +13,19 @@ public class ProductTest {
         this.product.setProductName("Sampo Cap Bambang");
         this.product.setProductQuantity(100);
     }
+
     @Test
     void testGetProductId(){
         assertEquals("eb558e9f-1c39-460e-8860-71af6af63bd6", this.product.getProductId());
+    }
+
+    @Test
+    void testGetProductName(){
+        assertEquals("Sampo Cap Bambang", this.product.getProductName());
+    }
+
+    @Test
+    void testGetProductQuantity(){
+        assertEquals(100, this.product.getProductQuantity());
     }
 }
