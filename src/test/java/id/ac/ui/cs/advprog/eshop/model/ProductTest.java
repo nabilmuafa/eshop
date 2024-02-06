b663095826ead88e5969e25d49a3c232e274cc44
nabilmuafa <nmuafa@gmail.com>
2024-02-06 23:57:05 +0700

    finish ProductRepositoryTest

--- src/test/java/id/ac/ui/cs/advprog/eshop/model/ProductTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/ProductTest.java
@@ -5,27 +5,43 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 public class ProductTest {
-    Product product;
+    Product product_valid;
+    Product product_invalid;
     @BeforeEach
     void setUp() {
-        this.product = new Product();
-        this.product.setProductId("eb558e9f-1c39-460e-8860-71af6af63bd6");
-        this.product.setProductName("Sampo Cap Bambang");
-        this.product.setProductQuantity(100);
+        this.product_valid = new Product();
+        this.product_valid.setProductId("eb558e9f-1c39-460e-8860-71af6af63bd6");
+        this.product_valid.setProductName("Sampo Cap Bambang");
+        this.product_valid.setProductQuantity(100);
+
+        this.product_invalid = new Product();
+        this.product_invalid.setProductId("eb558e9f-1c39-460e-8860-71af6af63bd6");
+        this.product_invalid.setProductName("");
+        this.product_invalid.setProductQuantity(-100);
     }
 
     @Test
     void testGetProductId(){
-        assertEquals("eb558e9f-1c39-460e-8860-71af6af63bd6", this.product.getProductId());
+        assertEquals("eb558e9f-1c39-460e-8860-71af6af63bd6", this.product_valid.getProductId());
     }
 
     @Test
     void testGetProductName(){
-        assertEquals("Sampo Cap Bambang", this.product.getProductName());
+        assertEquals("Sampo Cap Bambang", this.product_valid.getProductName());
     }
 
     @Test
     void testGetProductQuantity(){
-        assertEquals(100, this.product.getProductQuantity());
+        assertEquals(100, this.product_valid.getProductQuantity());
+    }
+
+    @Test
+    void testGetProductNameInvalid(){
+        assertEquals("Unknown", this.product_invalid.getProductName());
+    }
+
+    @Test
+    void testGetProductQuantityInvalid(){
+        assertEquals(1, this.product_invalid.getProductQuantity());
     }
 }
