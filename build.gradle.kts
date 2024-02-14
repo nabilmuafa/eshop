8e896eb296f6212e37cbd6e961e1582fe9a5fc83
nabilmuafa <nmuafa@gmail.com>
2024-02-14 09:56:19 +0700

    fix: sonarqube version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
 	java
 	jacoco
-	id("org.sonarqube") version "3.5.0.2730"
+	id("org.sonarqube") version "4.4.1.3373"
 	id("org.springframework.boot") version "3.2.2"
 	id("io.spring.dependency-management") version "1.1.4"
 }
