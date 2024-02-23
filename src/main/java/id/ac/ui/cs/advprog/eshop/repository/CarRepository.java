6dbc691d22495e4bb2d1bd1e3c7186b4a1759963
nabilmuafa <nmuafa@gmail.com>
2024-02-23 10:07:16 +0700

    feat: implement SRP to CarRepository

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/CarRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/CarRepository.java
@@ -1,5 +1,6 @@
 package id.ac.ui.cs.advprog.eshop.repository;
 import id.ac.ui.cs.advprog.eshop.model.Car;
+import id.ac.ui.cs.advprog.eshop.utils.UUIDGenerator;
 import org.springframework.stereotype.Repository;
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -9,11 +10,11 @@ import java.util.UUID;
 @Repository
 public class CarRepository {
     static int id = 0;
-    private List<Car> carData = new ArrayList<>();
+    private final List<Car> carData = new ArrayList<>();
+    private final UUIDGenerator uuidGenerator = new UUIDGenerator();
     public Car create(Car car){
         if(car.getCarId() == null){
-            UUID uuid = UUID.randomUUID();
-            car.setCarId(uuid.toString());
+            car.setCarId(uuidGenerator.generateId());
         }
         carData.add(car);
         return car;
