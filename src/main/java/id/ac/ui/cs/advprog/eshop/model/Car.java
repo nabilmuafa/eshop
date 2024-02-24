f495b347dfeef7a2ce6d580146eea5b20e501273
nabilmuafa <nmuafa@gmail.com>
2024-02-24 16:40:35 +0700

    refactor: add update method to Car

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Car.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Car.java
@@ -8,4 +8,10 @@ public class Car {
     private String carName;
     private String carColor;
     private String carQuantity;
+
+    public void update(Car car){
+        this.setCarName(car.getCarName());
+        this.setCarColor(car.getCarColor());
+        this.setCarQuantity(car.getCarQuantity());
+    }
 }
