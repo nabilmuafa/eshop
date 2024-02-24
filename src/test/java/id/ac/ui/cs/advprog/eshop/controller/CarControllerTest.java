e3f639106d20ae0a894a05dec606f6ea11333afd
nabilmuafa <nmuafa@gmail.com>
2024-02-24 18:11:28 +0700

    add: tests for Car

---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/controller/CarControllerTest.java
@@ -0,0 +1,53 @@
+package id.ac.ui.cs.advprog.eshop.controller;
+
+import id.ac.ui.cs.advprog.eshop.model.Car;
+import id.ac.ui.cs.advprog.eshop.service.CarServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ExtendWith(MockitoExtension.class)
+@WebMvcTest(controllers = CarController.class)
+@AutoConfigureMockMvc(addFilters = false)
+class CarControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private CarServiceImpl service;
+
+    @Test
+    void testGetListPage() throws Exception {
+        mockMvc.perform(get("/car/listCar"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("carList"))
+                .andExpect(model().attributeExists("cars"));
+    }
+
+    @Test
+    void testGetCreatePage() throws Exception {
+        mockMvc.perform(get("/car/createCar"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("createCar"))
+                .andExpect(model().attributeExists("car"));
+    }
+
+    @Test
+    void testPostCreatePage() throws Exception {
+        mockMvc.perform(post("/car/createCar"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:listCar"));
+    }
+}
