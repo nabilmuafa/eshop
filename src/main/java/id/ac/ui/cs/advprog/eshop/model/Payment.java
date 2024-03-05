105b1234f8e3c19069dc05a91f0c07f31cd8b55e
nabilmuafa <nmuafa@gmail.com>
2024-03-06 00:22:50 +0700

    [GREEN] Implement Payment model

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -3,6 +3,7 @@ package id.ac.ui.cs.advprog.eshop.model;
 import lombok.Setter;
 import lombok.Getter;
 
+import java.util.Arrays;
 import java.util.Map;
 
 @Getter
@@ -12,5 +13,30 @@ public class Payment {
     private String method;
     private String status;
     private Map<String, String> paymentData;
-    public Payment(String id, String method, String status, Map<String, String> paymentData) {}
+    public Payment(String id, String method, String status, Map<String, String> paymentData) {
+        this.id = id;
+        this.paymentData = paymentData;
+        this.setStatus(status);
+        this.setMethod(method);
+    }
+
+    public void setStatus(String status) {
+        String[] statusList = {"SUCCESS", "REJECTED"};
+        if (Arrays.stream(statusList).noneMatch(item -> (item.equals(status)))) {
+            throw new IllegalArgumentException();
+        }
+        else {
+            this.status = status;
+        }
+    }
+
+    public void setMethod(String method) {
+        String[] methodList = {"by-voucher", "by-transfer"};
+        if (Arrays.stream(methodList).noneMatch(item -> (item.equals(method)))) {
+            throw new IllegalArgumentException();
+        }
+        else {
+            this.method = method;
+        }
+    }
 }
