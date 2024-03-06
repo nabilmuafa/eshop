f93a6572bacd9fa01e675f6c68d4f4983b196230
nabilmuafa <nmuafa@gmail.com>
2024-03-06 20:38:39 +0700

    [GREEN] Implement PaymentByVoucher subfeature

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/PaymentByVoucher.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/PaymentByVoucher.java
@@ -15,6 +15,21 @@ public class PaymentByVoucher extends Payment{
 
     @Override
     public void setPaymentData(Map<String, String> paymentData) {
-
+        if (paymentData.isEmpty()) {
+            throw new IllegalArgumentException();
+        } else {
+            int nums = 0;
+            for (char c : paymentData.get("voucherCode").toCharArray()) {
+                if (Character.isDigit(c)) {
+                    nums++;
+                }
+            }
+            this.paymentData = paymentData;
+            if (paymentData.get("voucherCode").length() == 16 && paymentData.get("voucherCode").startsWith("ESHOP") && nums == 8) {
+                this.status = PaymentStatus.SUCCESS.getValue();
+            } else {
+                this.status = PaymentStatus.REJECTED.getValue();
+            }
+        }
     }
 }
